fd0578c8f0db004d42bf26e6c134de3370b0cea8
Noha Shehab <nohashehab.iti@gmail.com>
2023-06-05 13:50:32 +0300

    crud on data base

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 # import Flask ?
 
 from flask import Flask
-from flask import request
+from flask import request, redirect, url_for
 from flask import  render_template
+from flask_sqlalchemy import SQLAlchemy
 
 
 # ### create your first app
 app = Flask(__name__)  ## __name__  ==> python assign this variable with module name
 
+#### connect to database
+# tell the application the database path you need
 
+## http://www.ggogle.com/mm
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+db = SQLAlchemy(app)  # you are now connected sqlite
+# instance folder---> contain database --->
+
+###########################################################
 # when you check it --> from the module itself? ---> output __main__
 
 # ################## define first url in your application #####################
@@ -82,6 +91,60 @@ def get_student(id):
     return render_template("pagenotfound.html"), 404
 
 
+#################### Model Section ##################
+class Product(db.Model):
+    __tablename__= 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    price = db.Column(db.Integer)
+
+# to apply model content to the database
+# go to flask shell
+# db.create_all() =-->
+
+
+# ############ routes for crud on products
+@app.route("/products", endpoint='products.index')
+def get_all_products():
+    products = Product.query.all()
+    return render_template("products/index.html", products=products)
+
+
+@app.route("/products/<int:id>", endpoint="products.show")
+def get_product(id):
+    product = Product.query.get_or_404(id)
+    return render_template("products/show.html", product = product)
+
+
+@app.route("/products/<int:id>/delete", endpoint="products.delete")
+def delete_product(id):
+    product = Product.query.get_or_404(id)
+    db.session.delete(product)
+    db.session.commit()
+    url  = url_for('products.index')
+    return redirect(url)
+
+    # product = db.get_or_404(Product, id)
+    # db.session.delete(product)
+    # db.session.commit()
+    # return redirect(url_for("products.index"))
+
+
+@app.route("/products/create", methods = ["GET", "POST"],endpoint="products.create")
+def create_product():
+    ## request method POST
+    if request.method=="POST":
+        print(request.form)
+        product_name= request.form["name"]
+        product_price = request.form["price"]
+        product = Product(name=product_name, price=product_price)
+        db.session.add(product)
+        db.session.commit()
+
+        return redirect(url_for('products.index'))
+
+    ## request method GET ?
+    return render_template("products/create.html")
 
 
 
